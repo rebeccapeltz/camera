7b13c858203e8860af555e44ffa9d83bd900c4c5
Rebecca Peltz <rebeccapeltz@gmail.com>
2018-08-31 14:34:25 -0700

    working on final project with startup scripts

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ WSS.broadcast = function broadcast( data ) {
     });
 };
 
-var NodeWebcam = require( "./../../" );
+var NodeWebcam = require( "./index.js" );
 var Webcam = NodeWebcam.create({
     callbackReturn: "base64",
     saveShots: false
---
+++ Readme.md
@@ -0,0 +1,2 @@
+* Camera Capture and Filter Images
+
